7f7b71ede2fcd591866786f0e60fa759a10bb00c
Jeb Bearer <jeb.bearer@gmail.com>
2023-04-05 15:20:05 -0700

    Set up repo

---
+++ README.md
@@ -0,0 +1,53 @@
+Social bill tracker and civil engagement platform.
+
+# Development
+
+## Get the code
+
+```bash
+git clone git@github.com:jbearer/bill-tracker.git
+cd bill-tracker
+```
+
+## Install Nix
+
+This project consists of both a Rust project and a React project, each with their own dependencies.
+It uses [Nix](https://nixos.org/) as a one-stop shop for managing the complexity of these
+dependencies. While it is possible to develop on this project without Nix, by manually installing
+[Cargo](https://doc.rust-lang.org/cargo/), [Node.js](https://nodejs.org/en), and related
+dependencies, we highly recommend using Nix to get everything in one shot and be sure you are using
+the same versions as other developers.
+
+If you don't already have Nix on your system, install it by following the instructions
+[here](https://nixos.org/download.html). Once installed, you can enter a shell with up-to-date
+versions of all the necessary dependencies in scope simply by running `nix shell` from the project
+root.
+
+Optionally, you can also install [direnv](https://direnv.net/), which will automatically drop you into the Nix
+shell whenever you enter the project directory. Use `direnv allow` to enable this convenience after
+installing. Once in the Nix shell, you can use `direnv deny` if you ever need to drop out of it.
+
+## Building the Rust projects
+
+The whole project is a Cargo workspace, so to build everything you can simply run
+
+```bash
+cargo build --workspace
+```
+
+Use `cargo clippy` to run the linter and `cargo test` to run unit tests.
+
+## Building the React client
+
+There is a browser client for the app in `clients/browser`. This is a React Typescript project. To
+use it you should first cd in to the directory:
+
+```bash
+cd clients/browser
+```
+
+Then you can use various `npm` scripts to develop:
+* `npm start` will build a development version of the project, open it in your browser, and watch
+  your local directory for code changes
+* `npm test` runs unit tests
+* `npx eslint --fix` finds and fixes formatting and style issues
---
+++ flake.nix
@@ -0,0 +1,71 @@
+{
+  description = "Social bill tracker and civil engagement platform";
+
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs.flake-compat.url = "github:edolstra/flake-compat";
+  inputs.flake-compat.flake = false;
+  inputs.rust-overlay.url = "github:oxalica/rust-overlay";
+
+  outputs = { self, nixpkgs, flake-utils, flake-compat, rust-overlay }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [(import rust-overlay)];
+        pkgs = import nixpkgs { inherit system overlays; };
+        rustToolchain = pkgs.rust-bin.stable.latest.minimal.override {
+          extensions = [ "rustfmt" "clippy" "llvm-tools-preview" "rust-src" ];
+          targets = [ "wasm32-unknown-unknown" ];
+        };
+        nightlyToolchain = pkgs.rust-bin.nightly."2022-11-01".minimal.override {
+          extensions = ["rustfmt" "clippy" "llvm-tools-preview" "rust-src" ];
+          targets = [ "wasm32-unknown-unknown" ];
+        };
+        nixWithFlakes = pkgs.writeShellScriptBin "nix" ''
+          exec ${pkgs.nixVersions.stable}/bin/nix --experimental-features "nix-command flakes" "$@"
+        '';
+        rustDeps = with pkgs;
+            [
+              openssl
+              bash
+              curl
+              cargo-llvm-cov
+              cargo-sort
+              wasm-pack
+              openssl
+              binaryen
+              clang
+            ] ++ lib.optionals stdenv.isDarwin [
+              # required to build some crates on MacOS
+              darwin.apple_sdk.frameworks.Security
+              darwin.apple_sdk.frameworks.CoreFoundation
+            ];
+        nodeDeps = with pkgs;
+            [
+              nodejs
+            ];
+      in {
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs;
+          [
+            nixWithFlakes
+            git
+            rustToolchain
+          ] ++ rustDeps ++ nodeDeps;
+          shellHook = "export CC=${pkgs.clang}/bin/clang";
+          OPENSSL_INCLUDE_DIR = "${pkgs.openssl.dev}/include";
+          OPENSSL_LIB_DIR = "${pkgs.openssl.out}/lib";
+          RUST_LOG = "info";
+        };
+        devShells = {
+          nightlyShell = pkgs.mkShell {
+            buildInputs = with pkgs;
+            [
+              nixWithFlakes
+              nightlyToolchain
+              cargo-expand
+            ] ++ rustDeps ++ nodeDeps ;
+            RUST_LOG = "info";
+          };
+        };
+      }
+    );
+}
