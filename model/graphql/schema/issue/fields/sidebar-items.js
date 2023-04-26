258bc596ba9b98790a163fc1079d7be3830f3d26
jbearer <jbearer@users.noreply.github.com>
2023-04-26 23:57:34 +0000

    deploy: 3803e91e18f5767300a294b4c580c6d32866517a

--- model/graphql/schema/issue/fields/sidebar-items.js
+++ model/graphql/schema/issue/fields/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"struct":[["Bills","Metadata about the field Issue::Bills."],["Name","Metadata about the field Issue::Name."]]};
+window.SIDEBAR_ITEMS = {"struct":[["Bills","Metadata about the field Issue::Bills."],["Id","Metadata about the field Issue::Id."],["Name","Metadata about the field Issue::Name."]]};
