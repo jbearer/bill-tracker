f1b6edac262d199ef11e0e6f6408db4d7efb82c7
Jeb Bearer <jeb.bearer@gmail.com>
2023-05-08 08:40:16 -0700

    Add color scheme to navbar
    This adds a bit of color to the page and makes it easier to tell
    what the site is going to look like when we choose a color scheme.

--- clients/browser/src/components/nav-bar.tsx
+++ clients/browser/src/components/nav-bar.tsx
@@ -2,24 +2,36 @@ import React from 'react'
 import { Form, Link } from 'react-router-dom'
 import { createUseStyles } from 'react-jss'
 
-const useStyles = createUseStyles((theme) => ({
+import { type Theme } from 'themes/theme'
+
+const useStyles = createUseStyles((theme: Theme) => ({
   navBar: {
     display: 'flex',
-    justifyContent: 'space-between'
+    justifyContent: 'space-between',
+    backgroundColor: theme.color.primaryDark,
+    color: theme.color.on.primaryDark
   },
   navItem: {
-    margin: '10px 25px'
+    margin: '10px 25px',
+    display: 'flex'
   },
   searchItem: {
     extend: 'navItem',
     flex: '0 1 700px'
+  },
+  logo: {
+    textDecoration: 'none',
+    backgroundColor: theme.color.secondary,
+    color: theme.color.on.secondary
   },
   searchBar: {
     width: '100%',
     padding: '5px',
     margin: '2px',
     borderRadius: '12px',
-    fontSize: 24
+    fontSize: 24,
+    backgroundColor: theme.color.primaryLight,
+    color: theme.color.on.primaryLight
   }
 }))
 
@@ -29,7 +41,7 @@ export default function NavBar (): JSX.Element {
   return (
     <nav className={classes.navBar}>
       <div className={classes.navItem}>
-        <Link to="/">Logo</Link>
+        <Link className={classes.logo} to="/">Logo</Link>
       </div>
       <Form className={classes.searchItem} action="/search">
         <input type="text" name="query" placeholder="Search" className={classes.searchBar}/>
