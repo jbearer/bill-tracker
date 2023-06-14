9ab8b68b119d71d7bb876cb1c3020000af9a380f
jbearer <jbearer@users.noreply.github.com>
2023-06-14 00:41:09 +0000

    deploy: f3edabfbd0ed24e23d2ddab39a1311acadb2a32d

--- implementors/serde/de/trait.Deserialize.js
+++ implementors/serde/de/trait.Deserialize.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"model":[["impl&lt;'de&gt; <a class=\"trait\" href=\"https://docs.rs/serde/1.0.163/serde/de/trait.Deserialize.html\" title=\"trait serde::de::Deserialize\">Deserialize</a>&lt;'de&gt; for <a class=\"enum\" href=\"model/legiscan/enum.State.html\" title=\"enum model::legiscan::State\">State</a>"]]
+"model":[["impl&lt;'de&gt; <a class=\"trait\" href=\"https://docs.rs/serde/1.0.164/serde/de/trait.Deserialize.html\" title=\"trait serde::de::Deserialize\">Deserialize</a>&lt;'de&gt; for <a class=\"enum\" href=\"model/legiscan/enum.State.html\" title=\"enum model::legiscan::State\">State</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
